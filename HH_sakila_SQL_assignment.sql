7c9d1801c0dae7c0d696cc62791b1634e4143b4d
HAMMOUR <hishamhammour@gmail.com>
2019-03-26 17:47:12 -0500

    updating and adding some queries

--- HH_sakila_SQL_assignment.sql
+++ HH_sakila_SQL_assignment.sql
@@ -1,5 +1,6 @@
 -- SQL Assignment: 
 -- Using sakila Data Base
+
 USE sakila;
 
 -- 1a. Display First and Last Name of all actors:
@@ -11,7 +12,6 @@ SELECT CONCAT(UPPER(first_name), ' ', UPPER(last_name)) AS 'Actor Name'
 FROM actor;
 
 -- 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
-
 SELECT actor_id, first_name, last_name
 FROM actor
 WHERE actor.first_name LIKE 'Joe';
@@ -33,11 +33,11 @@ ORDER BY last_name , first_name;
 SELECT country_id, country
 FROM country
 WHERE country.country IN ('Afghanistan' , 'Bangladesh', 'China');
+
 -- 3a. You want to keep a description of each actor.
 -- You don't think you will be performing queries on a description,
 -- so create a column in the table actor named description and use the data type BLOB
 --(Make sure to research the type BLOB, as the difference between it and VARCHAR are significant).
- 
 ALTER TABLE actor
 ADD COLUMN description BLOB
 AFTER last_name;
@@ -127,3 +127,106 @@ FROM payment LEFT JOIN customer
 ON customer.customer_id = payment.customer_id
 GROUP BY customer.first_name , customer.last_name
 ORDER BY customer.last_name ASC;
+
+-- 7a. The music of Queen and Kris Kristofferson have seen an unlikely
+-- resurgence. As an unintended consequence, films starting
+-- with the letters K and Q have also soared in popularity.
+-- Use subqueries to display the titles of movies starting
+-- with the letters K and Q whose language is English.
+SELECT film.title
+FROM film
+WHERE film.language_id =
+        (SELECT language_id
+        FROM language
+        WHERE language.name = 'English')
+    AND film.title LIKE 'K%'
+    OR film.title LIKE 'Q%';
+
+-- 7b. Use subqueries to display all actors who appear in the 
+-- film Alone Trip.
+SELECT actor.first_name, actor.last_name
+FROM actor
+WHERE actor.actor_id = 
+	ANY (SELECT actor_id
+			FROM film_actor
+			WHERE film_actor.film_id =
+				(SELECT film_id
+					FROM film
+					WHERE film.title = 'Alone Trip'));
+
+-- 7c. You want to run an email marketing campaign in Canada,
+-- for which you will need the names and email addresses of all Canadian customers.
+-- Use joins to retrieve this information.
+SELECT customer.first_name, customer.last_name, customer.email
+FROM customer
+        INNER JOIN
+    address ON address.address_id = customer.address_id
+        INNER JOIN
+    city ON address.city_id = city.city_id
+        INNER JOIN
+    country ON city.country_id = country.country_id
+WHERE country.country = 'Canada';
+
+-- 7d. Sales have been lagging among young families,
+-- and you wish to target all family movies for a promotion.
+-- Identify all movies categorized as family films.
+SELECT title
+FROM film
+WHERE film.film_id =
+		ANY (SELECT film_id
+				FROM film_category
+				WHERE film_category.category_id =
+                (SELECT category_id
+					FROM category
+					WHERE
+                    name = 'Family'));
+
+-- 7e. Display the most frequently rented movies in descending order.
+SELECT title, COUNT(rental.inventory_id) AS Times_Rented
+FROM film
+INNER JOIN inventory
+ON film.film_id = inventory.film_id
+INNER JOIN rental
+ON inventory.inventory_id = rental.inventory_id
+GROUP BY film.title
+ORDER BY Times_Rented DESC;
+
+-- 7f. Write a query to display how much business, in dollars,
+-- each store brought in.
+SELECT store.store_id, SUM(payment.amount) AS Revenue
+FROM store
+INNER JOIN staff
+ON store.store_id = staff.store_id
+INNER JOIN payment
+ON payment.staff_id = staff.staff_id
+GROUP BY store.store_id
+ORDER BY Revenue DESC;
+
+-- 7g. Write a query to display for each store its store ID, city,
+-- and country.
+SELECT store.store_id, city.city, country.country
+FROM store
+INNER JOIN address
+ON store.address_id = address.address_id
+INNER JOIN city 
+ON address.city_id = city.city_id
+INNER JOIN country
+ON city.country_id = country.country_id;
+
+-- 7h. List the top five genres in gross revenue in descending order.
+-- (Hint: you may need to use the following tables:
+-- category, film_category, inventory, payment, and rental.)
+
+SELECT category.name AS Genre, SUM(payment.amount) AS Revenue
+FROM category
+INNER JOIN film_category
+ON category.category_id = film_category.category_id
+INNER JOIN inventory
+ON film_category.film_id = inventory.film_id
+INNER JOIN rental
+ON inventory.inventory_id = rental.inventory_id
+INNER JOIN payment
+ON rental.rental_id = payment.rental_id
+GROUP BY category.name
+ORDER BY Revenue DESC
+LIMIT 5;
