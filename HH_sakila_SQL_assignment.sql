1a4e22bac7bbd4bdd49a533fcf80664cfe9770ae
HAMMOUR <hishamhammour@gmail.com>
2019-03-28 08:44:42 -0500

    added the CREATE VIEW query and DROP

--- HH_sakila_SQL_assignment.sql
+++ HH_sakila_SQL_assignment.sql
@@ -230,3 +230,31 @@ ON rental.rental_id = payment.rental_id
 GROUP BY category.name
 ORDER BY Revenue DESC
 LIMIT 5;
+
+
+--- 8a. In your new role as an executive, you would like to have an easy
+-- way of viewing the Top five genres by gross revenue. Use the solution from
+-- the problem above to create a view.
+
+CREATE VIEW top_5_genres_by_revenue AS 
+    SELECT category.name AS Genre, SUM(payment.amount) AS Revenue
+    FROM category
+    INNER JOIN film_category
+    ON category.category_id = film_category.category_id
+    INNER JOIN inventory
+    ON film_category.film_id = inventory.film_id
+    INNER JOIN rental
+    ON inventory.inventory_id = rental.inventory_id
+    INNER JOIN payment
+    ON rental.rental_id = payment.rental_id
+    GROUP BY category.name
+    ORDER BY Revenue DESC
+    LIMIT 5;
+
+-- 8b. How would you display the view that you created in 8a?
+
+SELECT * FROM top_5_genres_by_revenue;
+
+-- 8c. You find that you no longer need the view top_five_genres.
+-- Write a query to delete it.
+DROP VIEW top_5_genres_by_revenue;
